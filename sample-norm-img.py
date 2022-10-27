22d836e72f9b5314efd0c564d087ca03a7d8e7b5
yarakigit <ya.tech.go@gmail.com>
2022-10-27 21:54:18 +0900

    CUDAカーネルの終了を待つ関数を追加

--- sample-norm-img.py
+++ sample-norm-img.py
@@ -4,7 +4,7 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
 # import self custom pycuda module
-from self_pycuda_mod import div255_mod, BLOCK_DIV_NUM
+from self_pycuda_mod import div255_mod
 
 import numpy, time 
 ARRAY_SIZE = 416*416*3
@@ -23,11 +23,12 @@ a_gpu = cuda.mem_alloc(a.size * a.dtype.itemsize)
 cuda.memcpy_htod(a_gpu, a)# gpu mem -> cpu mem
 
 # pycude exe
-block   = (int(ARRAY_SIZE/BLOCK_DIV_NUM), 1, 1)      #  Block size 
-grid    = (BLOCK_DIV_NUM                , 1, 1)      # Grid size
+block   = (   96, 1, 1)      #  Block size 
+grid    = ( 5408, 1, 1)      # Grid size
 func = div255_mod.get_function("div255")
 time_sta = time.perf_counter() # time-cal
 func(a_gpu ,block=block , grid=grid, shared=0)
+pycuda.autoinit.context.synchronize() # Wait for kernel completion before host access
 gpu_cuda_time = time.perf_counter() - time_sta # time-cal
 
 # gpu mem -> cpu mem
@@ -39,3 +40,11 @@ diff_log = [False if abs(a_correct[n] - a_div255[n])>0.0001 else True for n in r
 print("false") if  False in diff_log else print("success")# time-cal
 
 print("time[s]\nnumpy-cpu : {:1.5f}\ncuda-gpu  : {:1.5f}".format(cpu_numpy_time, gpu_cuda_time))
+
+
+## Memo
+### Max dimension size of a thread block (x,y,z): (1024, 1024, 64)
+### Max dimension size of a grid size    (x,y,z): (2147483647, 65535, 65535)
+
+### 416*416*3[pixel]を並列に計算したい...
+
--- self_pycuda_mod.py
+++ self_pycuda_mod.py
@@ -1,7 +1,6 @@
 import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
-BLOCK_DIV_NUM = 2**10
 
 div255_mod = SourceModule("""
     __global__ void div255(float *cal_array)
